c056cf433f95424e8a3b74e666ca04aa9e5a052a
unyeo32 <unyeo32@sm.ac.kr>
2020-10-08 15:52:23 +0900

    exercise2

--- main.c
+++ main.c
@@ -3,19 +3,28 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
-	int starPrint()
+	int sumTwo(int a, int b)
 	{
-		int i;
-		for (i=0; i<10; i++)
-			printf("*");
+		return (a+b);
 	}
 	
-	starPrint();
-	starPrint();
-	starPrint();
+	int square(int n)
+	{
+		return (n*n);
+	}
 	
-	
+	int get_max(int x, int y)
+	{
+		if (x>y)
+			return x;
+		else
+			return y;
+	}
+	int main(int argc, char *argv[]){
 		
-	return 0;
-}
+		printf("sumTwo: %i\n", sumTwo(1,3));
+		printf("square: %i\n", square(5));
+		printf("get_max: %i\n", get_max(3,5));
+		
+		return 0;
+	}
